e7a6bbae0ab7fb0a79a4881a0199811f5d03c26d
e-espootin <e.espootin@gmail.com>
2024-11-05 18:27:43 +0100

    test6

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "terraform" {
-    source = "./terraform"
-    AWS_ACCESS_KEY_ID     = "xxx"
-    AWS_SECRET_ACCESS_KEY = "xxx"
+  source = "./terraform"
+  # AWS_ACCESS_KEY_ID     = "xxx"
+  # AWS_SECRET_ACCESS_KEY = "xxx"
 }
