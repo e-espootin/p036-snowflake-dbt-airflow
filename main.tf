bda77e6daf5aeed15ec7ca0ef7ed82cd9f894e63
e-espootin <e.espootin@gmail.com>
2024-11-05 18:22:58 +0100

    test5

--- main.tf
+++ main.tf
@@ -1,21 +1,5 @@
-provider "snowflake" {
-    alias   = "DBT_ROLE"
-    role  = "DBT_ROLE"
-    # alias       = "main"
-    # SNOWFLAKE_ACCOUNT     = var.SNOWFLAKE_ACCOUNT
-    # SNOWFLAKE_AUTHENTICATOR = var.SNOWFLAKE_AUTHENTICATOR
-    # SNOWFLAKE_PRIVATE_KEY = var.SNOWFLAKE_PRIVATE_KEY
-    # SNOWFLAKE_USER = var.SNOWFLAKE_USER
-
-    # account   = var.SNOWFLAKE_ACCOUNT
-    # username  = var.SNOWFLAKE_USER
-    # password  = var.SNOWFLAKE_PRIVATE_KEY
-    # role      = "DBT_ROLE"
-    # warehouse = "dbt_wh"
-    # database  = "dbt_db"
-    # schema    = "dbt_schema"
-}
-
-module "azterraform" {
-  source = "./terraform"
+module "terraform" {
+    source = "./terraform"
+    AWS_ACCESS_KEY_ID     = "xxx"
+    AWS_SECRET_ACCESS_KEY = "xxx"
 }
