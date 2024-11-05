66a60096831f4e7ec154f29c38b98634d29437f7
e-espootin <e.espootin@gmail.com>
2024-11-05 18:39:24 +0100

    test8

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -37,11 +37,13 @@ variable "service_dbt_user" {
 variable "AWS_ACCESS_KEY_ID" {
   description = "AWS_ACCESS_KEY_ID"
   type        = string
-  sensitive   = true
+  # sensitive   = true
+  default = "${{secrets.AWS_ACCESS_KEY_ID }}"
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
   description = "AWS_SECRET_ACCESS_KEY"
   type        = string
-  sensitive   = true
+  # sensitive   = true
+  default = "${{secrets.AWS_SECRET_ACCESS_KEY }}"
 }
